84c3a3e6e89cbfc6365bfaf7da86605b7f234b77
Sergey Albit <Sergey.albit@avanquest.com>
2023-04-02 22:17:55 +0300

    Home_work_2.py

---
+++ Home_work_2.py
@@ -0,0 +1,9 @@
+countries = ['Ukraine', 'Spain', 'Italy']
+capitals = {
+    'Ukraine': 'Kyiv',
+    'Spain': 'Madrid',
+    'Italy': 'Rome'
+}
+
+for country in countries:
+    print(country + ': ' + capitals[country])
